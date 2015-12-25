3d10ec40fd8edef0f344501d2c36e05c28291af7
Eric <eric.wu@yieldmo.com>
2015-12-25 17:19:09 -0500

    new state logic

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 class App extends React.Component{
-	render(){
-		return <div>Done</div>;
+	constructor(){
+        super()
+        this.state = {
+            txt:'thisis the state txt',
+            cat:0
+        }
+    }
+    update(e){
+        this.setState({txt: e.target.value})
+    }
+    render(){
+		return (
+            <div>
+                <input type="text" onChange={this.update.bind(this)} />
+                <h1>{this.state.txt}</h1>
+            </div>
+        );
 	}
 }
 
