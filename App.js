675bfa83cf0a3452781ff3d0b116714a60d7bb4d
Eric <eric.wu@yieldmo.com>
2015-12-26 00:17:43 -0500

    Mutiple components

--- App.js
+++ App.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    update = (e) => {
+        this.setState({
+            value:e.target.value
+        });
+    }
     render() {
-        return <div>Done</div>;
+        return (
+            <div>
+                <input type="text" onChange={this.update} />
+                <Comp {...this.state} />
+                <Comp {...this.state} />
+                <Comp {...this.state} />
+                <Comp {...this.state} />
+            </div>
+        );
     }
 }
 
+
+const Comp = (props)=> {
+    return (
+        <div>{props.value}</div>
+    );
+}
 ReactDOM.render(<App />, document.getElementById('app'));
 
 
