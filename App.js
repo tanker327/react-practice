0a605b17ef7e07f981ea5eccf8da081280c816f9
Eric <eric.wu@yieldmo.com>
2015-12-26 00:05:07 -0500

    move render page to app.js

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component{
-	render(){
-		return <div>Done</div>;
-	}
+class App extends React.Component {
+    render() {
+        return <div>Done</div>;
+    }
 }
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
 
 export default App;
