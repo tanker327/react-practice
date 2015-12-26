9e9d556617c94ebae36bef9510ebcb56f6382082
Eric <eric.wu@yieldmo.com>
2015-12-26 00:08:08 -0500

    state practise

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component{
-	constructor(){
-        super()
-        this.state = {
-            txt:'thisis the state txt',
-            cat:0
-        }
+class App extends React.Component {
+    constructor(props) {
+        super(props)
     }
-    update(e){
+
+    state = {
+        txt:'default value'
+    }
+
+    update = (e)=> {
         this.setState({txt: e.target.value})
     }
-    render(){
-		return (
+
+    render() {
+        return (
             <div>
-                <input type="text" onChange={this.update.bind(this)} />
+                <input type="text" onChange={this.update}/>
                 <h1>{this.state.txt}</h1>
             </div>
         );
 	}
-
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
